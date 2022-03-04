be8ea764ac01434adf84d55e4f2276baf0824c6d
Thiago Shinji Assakawa Sakata <95872475+ShinjiSKA@users.noreply.github.com>
2022-03-04 20:22:48 -0300

    Add files via upload

---
+++ CreateFile/Program.cs
@@ -0,0 +1,37 @@
+﻿using static System.Console;
+
+WriteLine("File name: ");
+
+var name = ReadLine();
+name = ClearName(name);
+var path = Path.Combine(Environment.CurrentDirectory, $"{name}.txt");
+
+CreateFile(path);
+
+WriteLine("Press Enter to end...");
+ReadLine();
+
+static string ClearName(string name)
+{
+    foreach (var item in Path.GetInvalidFileNameChars())
+    {
+        name = name.Replace(item, '-');
+    }
+    return name;
+}
+static void CreateFile(string path)
+{
+    try
+    {
+
+        using var sw = File.CreateText(path);
+        sw.WriteLine("Line 1");
+        sw.WriteLine("Line 2");
+        sw.WriteLine("Line 3");
+    }
+
+    catch
+    {
+        WriteLine("Invalid Name!");
+    }
+}
